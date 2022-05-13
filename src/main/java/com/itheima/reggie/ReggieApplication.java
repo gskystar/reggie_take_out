7ebb991fece97d7d36ceed701d5a1de6520b7df5
gskystar <gliberte@163.com>
2022-05-13 14:42:31 +0800

    one test

---
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -0,0 +1,17 @@
+package com.itheima.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author agong
+ * @date 2022/5/13 14:23
+ */
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+    }
+}
